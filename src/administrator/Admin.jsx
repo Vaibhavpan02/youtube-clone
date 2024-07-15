f1fd0b7db38acf993c306993970b86413a5cf244
Vaibhavpan02 <pandeyvaibhav513@gmail.com>
2024-07-15 23:31:55 +0530

    commit

---
+++ src/administrator/Admin.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <a
+      className="admin"
+      href="https://www.github.com/sumyat-aung"
+      target="_blank"
+    >
+      Created by Vaibhav Pandey
+    </a>
+  );
+};
+
+export default Admin;
---
+++ src/key/key.js
@@ -0,0 +1,3 @@
+const API_KEY = "4c9243f7demshaca37c8a0f78015p111010jsn0117aaf31599";
+
+export { API_KEY };
